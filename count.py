198e524badd5c998364e9b57c800dac057dd71e9
Marta Tyzhnyk <tuznuk.marta@gmail.com>
2019-11-30 17:29:34 +0200

    new Count of positives / sum of negatives

--- count.py
+++ count.py
@@ -1,4 +1,4 @@
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
+arr = [0,0,0,0,0,0,0,0,0]
 def count_positives_sum_negatives(arr):
   count = 0
   suma = 0
@@ -7,8 +7,12 @@ def count_positives_sum_negatives(arr):
       count += 1
     else:
       suma += i
+  l1 = [0, 0]
   l = []
   l.append(count)
   l.append(suma)
-  return l
+  if i == 0:
+      return l1
+  else:
+      return l
 print(count_positives_sum_negatives(arr))
